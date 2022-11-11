2638e8837affd7afbd5bbc3d8de06205032f9181
AndyC_WC <45092498+andyc1997@users.noreply.github.com>
2022-11-11 11:44:14 +0100

    code for running repeated experiments

---
+++ r0123456_repeated.py
@@ -0,0 +1,200 @@
+import random
+from datetime import datetime
+from typing import NamedTuple
+from tqdm import tqdm
+
+import numpy as np
+import Reporter
+
+
+
+class Individual(NamedTuple):
+    tour: list[int]
+    cost: float
+
+
+class r0123456:
+    def __init__(self, seed: int):
+        self.reporter = Reporter.Reporter(self.__class__.__name__)
+        self.rng = np.random.default_rng(seed=seed)
+        self.bestObjective = np.inf
+
+    def initial_population(self, distanceMatrix, n: int) -> list[Individual]:
+        problem_size = distanceMatrix.shape[0]
+        individuals = []
+        for i in range(n):
+            tour = np.random.default_rng().permutation(problem_size)
+            cost = self.cost(tour, distanceMatrix)
+            new_individual = Individual(tour=tour, cost=cost)
+            individuals.append(new_individual)
+        return individuals
+
+    def cost(self, tour: list[int], distanceMatrix) -> float:
+        return np.sum(distanceMatrix[tour, np.roll(tour, -1)])
+
+    def parent_selection(
+            self, population: list[Individual], n: int
+    ) -> list[Individual]:
+        """Random parent selection with size N"""
+        return [
+            random.choice(population) for _ in range(n)
+        ]
+
+    @staticmethod
+    def ordered_crossover(parent1, parent2, distanceMatrix):
+        pt1 = list(parent1.tour)
+        pt2 = list(parent2.tour)
+        l = len(pt1)
+        cp1 = np.random.randint(0, len(pt1) - 1)
+        cp2 = np.random.randint(cp1 + 1, len(pt1))
+        pt1_relative_order = pt1[cp2:] + pt1[:cp2]
+        pt2_relative_order = pt2[cp2:] + pt2[:cp2]
+        ct1 = [n for n in pt1_relative_order if n not in pt2[cp1:cp2]]
+        ct2 = [n for n in pt2_relative_order if n not in pt1[cp1:cp2]]
+        ct1 = ct1[l - cp2:] + pt2[cp1:cp2] + ct1[:l - cp2]
+        ct2 = ct2[l - cp2:] + pt1[cp1:cp2] + ct2[:l - cp2]
+        child1 = Individual(tour = ct1, cost = r0123456.calculate_tour_cost(distanceMatrix, ct1))
+        child2 = Individual(tour = ct2, cost = r0123456.calculate_tour_cost(distanceMatrix, ct2))
+        return [child1, child2]
+
+    @staticmethod
+    def non_wrapping_ordered_crossover(parent1, parent2, distanceMatrix):
+        pt1 = list(parent1.tour)
+        pt2 = list(parent2.tour)
+        cp1 = np.random.randint(0, len(pt1) - 1)
+        cp2 = np.random.randint(cp1 + 1, len(pt1))
+        ct1 = [n for n in pt1 if n not in pt2[cp1:cp2]]
+        ct2 = [n for n in pt2 if n not in pt1[cp1:cp2]]
+        ct1 = ct1[:cp1] + pt2[cp1:cp2] + ct1[cp1:]
+        ct2 = ct2[:cp1] + pt1[cp1:cp2] + ct2[cp1:]
+        child1 = Individual(tour = ct1, cost = r0123456.calculate_tour_cost(distanceMatrix, ct1))
+        child2 = Individual(tour = ct2, cost = r0123456.calculate_tour_cost(distanceMatrix, ct2))
+        return [child1, child2]
+
+    def crossover(self, parents: list[Individual], distanceMatrix):
+        # Morph
+        # choose even number parents
+        pair_list = list(zip(parents[::2], parents[1::2]))
+        children = []
+        for i in range(len(pair_list)):
+            children.extend(
+                self.ordered_crossover(
+                    pair_list[i][0], pair_list[i][1], distanceMatrix
+                )
+            )
+        return children
+
+    @staticmethod
+    def calculate_tour_cost(distanceMatrix, tour) -> float:
+        cost = 0
+        for city_idx in range(-1, len(tour)):
+            next_city_idx = (city_idx+1) % len(tour)
+            cost += distanceMatrix[city_idx, next_city_idx]
+        return cost
+
+    def mutate(
+            self, distanceMatrix, offspring: list[Individual]
+    ) -> list[Individual]:
+        """Mutation using edge"""
+
+        mutation_rate = 0.3
+
+        new_offspring = []
+        for o in offspring:
+            if self.rng.random() > mutation_rate:
+                new_offspring.append(o)
+                continue
+
+            a, b = self.rng.choice(len(o.tour), 2, replace=False)
+            tour = np.array(o.tour)
+            tour[b], tour[a] = tour[a], tour[b]
+
+            tour_cost = self.cost(tour, distanceMatrix)
+            new_offspring.append(Individual(tour, tour_cost))
+        return new_offspring
+
+    def selection(
+            self,
+            population: list[Individual],
+            offspring: list[Individual],
+            n: int,
+            k: int
+    ) -> list[Individual]:
+        # k-tournament
+        pool = population + offspring
+        new_population = []
+        for i in range(n):
+            idx_sampled = self.rng.choice(len(pool), k, replace=False)  # k-tournament
+            pool_sampled = [pool[idx] for idx in idx_sampled]
+            pool_subset = sorted(pool_sampled, key=lambda x: x.cost)
+            new_population.append(pool_subset[0])
+        return new_population
+
+    # The evolutionary algorithm's main loop
+    def optimize(self, filename):
+        # Read distance matrix from file.
+        file = open(filename)
+        distanceMatrix = np.loadtxt(file, delimiter=",")
+        file.close()
+
+        population = self.initial_population(distanceMatrix, n=50)
+        print('Pop intialized!')
+
+        while True:
+            # Your code here.
+            # parents is a list of cycles, costs is a list of real numbers
+            parents = self.parent_selection(population, n=50)
+            offspring = self.crossover(parents, distanceMatrix)
+            new_offspring = self.mutate(distanceMatrix, offspring)
+            population = self.selection(population, new_offspring, n=50, k=3)
+
+            meanObjective = np.mean([
+                individual.cost for individual in population
+                if np.isfinite(individual.cost)
+            ])
+            valid = sum((
+                1
+                for individual in population
+                if np.isfinite(individual.cost)
+            ))
+
+            bestIdx = np.argmin([ individual.cost for individual in population])
+
+            bestObjective = population[bestIdx].cost
+            bestSolution = population[bestIdx].tour
+
+
+            # Call the reporter with:
+            #  - the mean objective function value of the population
+            #  - the best objective function value of the population
+            #  - a 1D numpy array in the cycle notation containing the best solution
+            #    with city numbering starting from 0
+            timeLeft = self.reporter.report(
+                meanObjective, bestObjective, bestSolution)
+            # print(
+            #     'Best / mean(only finite) / valid '
+            #     f'{bestObjective:.0f} / {meanObjective:.0f} / {valid}  -- '
+            #     f'{timeLeft:.1f} seconds left!'
+            # )
+            if bestObjective < self.bestObjective:
+                self.bestObjective = bestObjective
+            if timeLeft < 0:
+                break
+
+        # Your code here.
+        return 0
+
+
+if __name__ == '__main__':
+    now = datetime.now()
+    current_time = now.strftime('%H%M%S')
+    n_runs = 20
+    seeds = np.random.randint(999, size=n_runs)
+    bestObjectives = np.zeros(n_runs)
+    for i, seed in enumerate(tqdm(seeds), 0):
+        solver = r0123456(seed=seed)
+        solver.optimize('tour50.csv')
+        bestObjectives[i] = solver.bestObjective
+    records = {'seeds': seeds, 'bestObjectives': bestObjectives}
+    np.save('best_objectives_record-'+current_time, records)
+
